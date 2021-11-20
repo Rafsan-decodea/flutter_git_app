0a7aa170786179a2d719cd8b1dc1ccb1da08b2b1
Your Name <you@example.com>
2021-11-20 12:21:23 +0600

    build new test_app

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'home.dart';
 
-
-void main()
-{
+void main() {
   runApp(homeApp());
 }
 
@@ -11,19 +12,30 @@ class homeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "home2",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello"),
+      home: Home(),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  CustomButton({
+    this.title,
+  });
+
+  dynamic title;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(80), bottomRight: Radius.circular(80)),
+      child: Card(
+        child: Container(
+          alignment: Alignment.center,
+          color: Colors.green,
+          child: Text(title, style: GoogleFonts.lato(fontSize: 24)),
         ),
-        body: Column(
-          
-          children: <Widget>[
-            Text(
-              "Hi"
-            ),
-          ],
-        ),
-      )
-    ) ;
+      ),
+    );
   }
 }
