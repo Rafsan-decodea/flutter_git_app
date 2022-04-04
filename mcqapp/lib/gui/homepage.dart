cd161bcdab0eb7e7437d81cc152457fc97815c23
MD Rafsan <shazidno123@gmail.com>
2022-04-04 16:40:13 +0600

    add Shop Flutter App

--- mcqapp/lib/gui/homepage.dart
+++ mcqapp/lib/gui/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:neon/neon.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,60 +14,64 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(150),
-        child: AppBar(
-            automaticallyImplyLeading: false,
-            backgroundColor: Colors.indigoAccent,
-            flexibleSpace: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Neon(
-                    text: 'MCQ Question BCS Question',
-                    color: Colors.cyan,
-                    fontSize: 25,
-                    font: NeonFont.Cyberpunk,
-                    textStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(170),
+          child: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.indigo,
+              flexibleSpace: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 50,
                     ),
-                    flickeringText: true,
-                    flickeringLetters: [0, 11],
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text("The Best Sloution from BCS ",
-                      style: GoogleFonts.aldrich(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white)),
-                  SizedBox(
-                    height: 1,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(9.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        RaisedButton(
-                          child: Text('Login '),
-                          onPressed: () {},
-                        ),
-                        RaisedButton(
-                          child: Text('Register'),
-                          onPressed: () {},
-                        ),
-                      ],
+                    Neon(
+                      text: 'MCQ Question BCS Question',
+                      color: Colors.cyan,
+                      fontSize: 20,
+                      font: NeonFont.Monoton,
+                      textStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                      flickeringText: true,
+                      flickeringLetters: [0, 11],
                     ),
-                  )
-                ],
-              ),
-            )),
-      ),
-    );
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text("The Best Sloution from BCS ",
+                        style: GoogleFonts.alexBrush(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)),
+                    SizedBox(
+                      height: 1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(9.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          RaisedButton(
+                            child: Text('Login '),
+                            onPressed: () {},
+                          ),
+                          RaisedButton(
+                            child: Text('Register'),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )),
+        ),
+        body: Column(
+          children: [
+            Text("Hello world"),
+          ],
+        ));
   }
 }
