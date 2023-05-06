50acfc0389fd2c5987bf8f6f70348ed8fefa55ad
MD Rafsan <shazidno123@gmail.com>
2023-05-06 20:25:42 +0600

    Developing Color

--- towhidsirapp/elaankraft/lib/view/screen/onboarding/onboarding_screen.dart
+++ towhidsirapp/elaankraft/lib/view/screen/onboarding/onboarding_screen.dart
@@ -23,8 +23,8 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Provider.of<OnBoardingProvider>(context, listen: false).initBoardingList(context);
-
+    Provider.of<OnBoardingProvider>(context, listen: false)
+        .initBoardingList(context);
 
     double _height = MediaQuery.of(context).size.height;
 
@@ -32,29 +32,41 @@ class OnBoardingScreen extends StatelessWidget {
       body: Stack(
         clipBehavior: Clip.none,
         children: [
-          Provider.of<ThemeProvider>(context).darkTheme ? SizedBox() : Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: Image.asset(Images.background, fit: BoxFit.fill),
-          ),
+          Provider.of<ThemeProvider>(context).darkTheme
+              ? SizedBox()
+              : Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Image.asset(Images.background, fit: BoxFit.fill),
+                ),
           Consumer<OnBoardingProvider>(
             builder: (context, onBoardingList, child) => ListView(
               children: [
                 SizedBox(
-                  height: _height*0.7,
+                  height: _height * 0.7,
                   child: PageView.builder(
                     itemCount: onBoardingList.onBoardingList.length,
                     controller: _pageController,
                     itemBuilder: (context, index) {
                       return Padding(
-                        padding: EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
+                        padding:
+                            EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
                         child: Column(
                           children: [
-                            Image.asset(onBoardingList.onBoardingList[index].imageUrl, height: _height*0.5),
-                            Text(onBoardingList.onBoardingList[index].title, style: titilliumBold.copyWith(fontSize: _height*0.035), textAlign: TextAlign.center),
-                            Text(onBoardingList.onBoardingList[index].description, textAlign: TextAlign.center, style: titilliumRegular.copyWith(
-                              fontSize: _height*0.015,
-                            )),
+                            Image.asset(
+                                onBoardingList.onBoardingList[index].imageUrl,
+                                height: _height * 0.5),
+                            Text(onBoardingList.onBoardingList[index].title,
+                                style: titilliumBold.copyWith(
+                                    fontSize: _height * 0.035),
+                                textAlign: TextAlign.center),
+                            Text(
+                                onBoardingList
+                                    .onBoardingList[index].description,
+                                textAlign: TextAlign.center,
+                                style: titilliumRegular.copyWith(
+                                  fontSize: _height * 0.015,
+                                )),
                           ],
                         ),
                       );
@@ -71,34 +83,54 @@ class OnBoardingScreen extends StatelessWidget {
                       padding: const EdgeInsets.only(bottom: 20),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: _pageIndicators(onBoardingList.onBoardingList, context),
+                        children: _pageIndicators(
+                            onBoardingList.onBoardingList, context),
                       ),
                     ),
                     Container(
                       height: 45,
-                      margin: EdgeInsets.symmetric(horizontal: 70, vertical: Dimensions.PADDING_SIZE_SMALL),
+                      margin: EdgeInsets.symmetric(
+                          horizontal: 70,
+                          vertical: Dimensions.PADDING_SIZE_SMALL),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(6),
                           gradient: LinearGradient(colors: [
-                            Theme.of(context).primaryColor,
-                            Theme.of(context).primaryColor,
-                            Theme.of(context).primaryColor,
+                            Color(0xFFD5AF37), //Theme.of(context).primaryColor,
+                            Color(0xFFD5AF37), //Theme.of(context).primaryColor,
+                            Color(0xFFD5AF37), //Theme.of(context).primaryColor,
                           ])),
                       child: TextButton(
                         onPressed: () {
-                          if (Provider.of<OnBoardingProvider>(context, listen: false).selectedIndex == onBoardingList.onBoardingList.length - 1) {
-                            Provider.of<SplashProvider>(context, listen: false).disableIntro();
-                            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => AuthScreen()));
+                          if (Provider.of<OnBoardingProvider>(context,
+                                      listen: false)
+                                  .selectedIndex ==
+                              onBoardingList.onBoardingList.length - 1) {
+                            Provider.of<SplashProvider>(context, listen: false)
+                                .disableIntro();
+                            Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => AuthScreen()));
                           } else {
-                            _pageController.animateToPage(Provider.of<OnBoardingProvider>(context, listen: false).selectedIndex+1, duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+                            _pageController.animateToPage(
+                                Provider.of<OnBoardingProvider>(context,
+                                            listen: false)
+                                        .selectedIndex +
+                                    1,
+                                duration: Duration(milliseconds: 300),
+                                curve: Curves.easeInOut);
                           }
                         },
                         child: Container(
                           width: double.infinity,
                           alignment: Alignment.center,
-                          child: Text(onBoardingList.selectedIndex == onBoardingList.onBoardingList.length - 1
-                              ? getTranslated('GET_STARTED', context) : getTranslated('NEXT', context),
-                              style: titilliumSemiBold.copyWith(color: Colors.white, fontSize: Dimensions.FONT_SIZE_LARGE)),
+                          child: Text(
+                              onBoardingList.selectedIndex ==
+                                      onBoardingList.onBoardingList.length - 1
+                                  ? getTranslated('GET_STARTED', context)
+                                  : getTranslated('NEXT', context),
+                              style: titilliumSemiBold.copyWith(
+                                  color: Color.fromARGB(255, 15, 1, 1),
+                                  fontSize: Dimensions.FONT_SIZE_LARGE)),
                         ),
                       ),
                     ),
@@ -118,12 +150,19 @@ class OnBoardingScreen extends StatelessWidget {
     for (int i = 0; i < onBoardingList.length; i++) {
       _indicators.add(
         Container(
-          width: i == Provider.of<OnBoardingProvider>(context).selectedIndex ? 18 : 7,
+          width: i == Provider.of<OnBoardingProvider>(context).selectedIndex
+              ? 18
+              : 7,
           height: 7,
           margin: EdgeInsets.only(right: 5),
           decoration: BoxDecoration(
-            color: i == Provider.of<OnBoardingProvider>(context).selectedIndex ? Theme.of(context).primaryColor : Colors.white,
-            borderRadius: i == Provider.of<OnBoardingProvider>(context).selectedIndex ? BorderRadius.circular(50) : BorderRadius.circular(25),
+            color: i == Provider.of<OnBoardingProvider>(context).selectedIndex
+                ? Theme.of(context).primaryColor
+                : Colors.white,
+            borderRadius:
+                i == Provider.of<OnBoardingProvider>(context).selectedIndex
+                    ? BorderRadius.circular(50)
+                    : BorderRadius.circular(25),
           ),
         ),
       );
