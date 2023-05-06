6d2c798ec32be1a9cc79d0537e03ba3693843575
MD Rafsan jani <shazidno123@gmail.com>
2023-05-07 03:56:37 +0600

    Editd Color

--- towhidsirapp/elaankraft/lib/view/screen/chat/widget/inbox_shimmer.dart
+++ towhidsirapp/elaankraft/lib/view/screen/chat/widget/inbox_shimmer.dart
@@ -3,8 +3,6 @@ import 'package:flutter_sixvalley_ecommerce/utill/color_resources.dart';
 import 'package:flutter_sixvalley_ecommerce/utill/dimensions.dart';
 import 'package:shimmer/shimmer.dart';
 
-
-
 class InboxShimmer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -22,7 +20,8 @@ class InboxShimmer extends StatelessWidget {
               CircleAvatar(child: Icon(Icons.person), radius: 30),
               Expanded(
                 child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: Dimensions.PADDING_SIZE_SMALL),
+                  padding: EdgeInsets.symmetric(
+                      horizontal: Dimensions.PADDING_SIZE_SMALL),
                   child: Column(children: [
                     Container(height: 15, color: ColorResources.WHITE),
                     SizedBox(height: Dimensions.PADDING_SIZE_EXTRA_SMALL),
@@ -36,7 +35,8 @@ class InboxShimmer extends StatelessWidget {
                 Container(
                   height: 15,
                   width: 15,
-                  decoration: BoxDecoration(shape: BoxShape.circle, color: Theme.of(context).primaryColor),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0xFFD5AF37)),
                 ),
               ])
             ]),
